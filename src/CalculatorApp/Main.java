853cb7c397b6df908fafc894dee699089d0ece30
ekabotdev <ekabotdev@gmail.com>
2026-04-16 15:41:16 +0300

    intial commit Upgraded to a  Loop-Based Calculator

--- src/CalculatorApp/Main.java
+++ src/CalculatorApp/Main.java
@@ -5,40 +5,50 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean running = true;
 
-        System.out.println("Enter first number: ");
-        double num1 = input.nextDouble();
+        while (running) {
+            System.out.println("Enter first number: ");
+            double num1 = input.nextDouble();
 
-        System.out.println("Enter operator (+, _, *, /)");
-        char operator = input.next().charAt(0);
+            System.out.println("Enter operator (+, _, *, /)");
+            char operator = input.next().charAt(0);
 
-        System.out.println("Enter second number: ");
-        double num2 = input.nextDouble();
+            System.out.println("Enter second number: ");
+            double num2 = input.nextDouble();
 
-        double result;
+            double result;
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
                 case '-':
                     result = num1 - num2;
                     break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                        case '/':
-                            if (num2 != 0) {
-                               result = num1 / num2;
-                            }else {
-                                System.out.println("Cannot divide by zero");
-                                return;
-                            }
-                            break;
-                            default:
-                                System.out.println("Invalid operator");
-                                return;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Cannot divide by zero");
+                        continue;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator");
+                    continue;
+            }
+            System.out.println("The result is: " + result);
+            System.out.println("Do you want to continue? (yes/no) :");
+            String choice = input.next();
+
+            if (choice.equalsIgnoreCase("no")) {
+                running = false;
+                System.out.println("Calculator exited.");
+            }
         }
-        System.out.println("The result is: " + result);
     }
 }
