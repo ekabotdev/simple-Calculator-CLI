45268056f5f14807543af9cc6c4a0412361e1142
ekabotdev <ekabotdev@gmail.com>
2026-04-17 14:01:19 +0300

    intial commit added a calculator class

--- src/CalculatorApp/Calculator.java
+++ src/CalculatorApp/Calculator.java
@@ -1,4 +1,25 @@
 package CalculatorApp;
 
 public class Calculator {
+    public Double calculate(double num1, double num2, char operator) {
+
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                    case '*':
+                        return num1 * num2;
+                        case '/':
+                            if (num2 != 0) {
+                                return num1 / num2;
+                            }else {
+                                System.out.println("Error: Cannot divide by zero");
+                                return null;
+                            }
+            default:
+                System.out.println("invalid operator");
+                return null;
+        }
+    }
 }
--- src/CalculatorApp/Main.java
+++ src/CalculatorApp/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
         boolean running = true;
 
         while (running) {
@@ -15,7 +16,7 @@ public class Main {
             char operator = getOperator(scanner);
             double num2 = getNumber(scanner, "Enter second number: ");
 
-            Double result = calculate(num1, num2, operator);
+            Double result = calculator.calculate(num1, num2, operator);
 
             if (result != null) {
                 System.out.println("Result: " + result);
@@ -41,8 +42,6 @@ public class Main {
 
         }
     }
-
-
     //  Method to get operator
     public static char getOperator(Scanner scanner) {
         while (true) {
@@ -56,35 +55,6 @@ public class Main {
             }
         }
     }
-
-
-    // Method to calculate result
-    public static Double calculate(double num1, double num2, char operator) {
-
-        switch (operator) {
-            case '+':
-                return num1 + num2;
-
-            case '-':
-                return num1 - num2;
-
-            case '*':
-                return num1 * num2;
-
-            case '/':
-                if (num2 != 0) {
-                    return num1 / num2;
-                } else {
-                    System.out.println("Error: Cannot divide by zero.");
-                    return null;
-                }
-
-            default:
-                System.out.println("Invalid operator.");
-                return null;
-        }
-    }
-
     //  Method to check if user wants to continue
     public static boolean shouldContinue(Scanner scanner) {
         System.out.print("Do you want to continue? (yes/no): ");
